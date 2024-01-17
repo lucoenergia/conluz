872a3bbc9ebf54e980c830c3dabda1b491983db0
Víctor Cañizares Mata <victor.canizares.mata@gmail.com>
2024-01-17 23:37:58 +0100

    [CONLUZ-44] Verified endpoint to enable a user (#48)

--- src/test/java/org/lucoenergia/conluz/infrastructure/admin/user/enable/EnableUserControllerTest.java
+++ src/test/java/org/lucoenergia/conluz/infrastructure/admin/user/enable/EnableUserControllerTest.java
@@ -10,10 +10,15 @@ import org.lucoenergia.conluz.domain.admin.user.UserMother;
 import org.lucoenergia.conluz.infrastructure.shared.BaseControllerTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
@@ -40,5 +45,38 @@ class EnableUserControllerTest extends BaseControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
         Assertions.assertTrue(getUserRepository.findByPersonalId(UserPersonalId.of(user.getPersonalId())).get().isEnabled());
+    }
+
+    @Test
+    void testWithUnknownUser() throws Exception {
+
+        String authHeader = loginAsDefaultAdmin();
+
+        final String userId = UUID.randomUUID().toString();
+
+        mockMvc.perform(post("/api/v1/users/" + userId + "/enable")
+                        .header(HttpHeaders.AUTHORIZATION, authHeader)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void testWithoutToken() throws Exception {
+
+        final String userId = UUID.randomUUID().toString();
+
+        mockMvc.perform(post("/api/v1/users/" + userId + "/enable")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
     }
 }
