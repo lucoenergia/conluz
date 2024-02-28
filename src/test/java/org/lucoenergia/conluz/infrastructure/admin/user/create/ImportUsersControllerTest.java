04caf0255fbcab622a8604b4fdf3b652a66da0e7
viktorKhan <victor.canizares.mata@gmail.com>
2024-02-28 22:57:44 +0100

    [conluz-52] Created endpoint to create users in bulk by importing a CSV file

--- src/test/java/org/lucoenergia/conluz/infrastructure/admin/user/create/ImportUsersControllerTest.java
+++ src/test/java/org/lucoenergia/conluz/infrastructure/admin/user/create/ImportUsersControllerTest.java
@@ -1,5 +1,7 @@
 package org.lucoenergia.conluz.infrastructure.admin.user.create;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 import org.lucoenergia.conluz.infrastructure.shared.BaseControllerTest;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.file.Files;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,7 +41,11 @@ class ImportUsersControllerTest extends BaseControllerTest {
                         .file(file)
                         .header(HttpHeaders.AUTHORIZATION, authHeader))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.created").isArray())
+                .andExpect(jsonPath("$.created").isNotEmpty())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors").isNotEmpty());
     }
 
     @Test
@@ -62,6 +69,129 @@ class ImportUsersControllerTest extends BaseControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.timestamp").isNotEmpty())
                 .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void testWithWrongFileMimeType() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("users.csv");
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "users.csv",
+                "application/octet-stream",
+                Files.readAllBytes(resource.getFile().toPath()));
+
+        String authHeader = loginAsDefaultAdmin();
+
+        mockMvc.perform(multipart(URL)
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION, authHeader))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void testWithWrongFileExtension() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("application-test.properties");
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "users.txt",
+                "text/csv",
+                Files.readAllBytes(resource.getFile().toPath()));
+
+        String authHeader = loginAsDefaultAdmin();
+
+        mockMvc.perform(multipart(URL)
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION, authHeader))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void testWithEmptyCsvFile() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("empty.csv");
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "users.csv",
+                "text/csv",
+                Files.readAllBytes(resource.getFile().toPath()));
+
+        String authHeader = loginAsDefaultAdmin();
+
+        mockMvc.perform(multipart(URL)
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION, authHeader))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.created").isArray())
+                .andExpect(jsonPath("$.created").isEmpty())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors").isEmpty());
+    }
+
+    @Test
+    void testWithMalformedCsvFile() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("users_malformed.csv");
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "users.csv",
+                "text/csv",
+                Files.readAllBytes(resource.getFile().toPath()));
+
+        String authHeader = loginAsDefaultAdmin();
+
+        mockMvc.perform(multipart(URL)
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION, authHeader))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void
+    testWithoutFile() throws Exception {
+        String authHeader = loginAsDefaultAdmin();
+
+        mockMvc.perform(post(URL)
+                        .header(HttpHeaders.AUTHORIZATION, authHeader))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.traceId").isNotEmpty());
+    }
+
+    @Test
+    void testWithoutToken() throws Exception {
+
+        mockMvc.perform(post(URL))
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
                 .andExpect(jsonPath("$.message").isNotEmpty())
                 .andExpect(jsonPath("$.traceId").isNotEmpty());
     }
