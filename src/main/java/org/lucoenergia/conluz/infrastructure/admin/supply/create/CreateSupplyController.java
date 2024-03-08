d021c7442fd270f10d2b7db81429964622a1110e
viktorKhan <victor.canizares.mata@gmail.com>
2024-03-08 17:27:11 +0100

    [conluz-54] Adapted OAS documentation to the adition of the new code field

--- src/main/java/org/lucoenergia/conluz/infrastructure/admin/supply/create/CreateSupplyController.java
+++ src/main/java/org/lucoenergia/conluz/infrastructure/admin/supply/create/CreateSupplyController.java
@@ -55,6 +55,7 @@ public class CreateSupplyController {
                             examples = @ExampleObject(
                                     value = """
                                             {
+                                               "id":"785de77b-8c22-4d2f-9d12-f172113f9aa4",
                                                "code":"ES0033333333333333AA0A",
                                                "user":{
                                                   "id":"e7ab39cd-9250-40a9-b829-f11f65aae27d",
