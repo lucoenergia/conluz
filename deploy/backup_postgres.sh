376defb537d07b439f6fc1b74f324aaa332f4f83
viktorKhan <victor.canizares.mata@gmail.com>
2025-06-23 23:55:23 +0200

    Fixed script to make a backup of postgres database

--- deploy/backup_postgres.sh
+++ deploy/backup_postgres.sh
@@ -7,19 +7,22 @@ FILENAME="${DB_NAME}_${TIMESTAMP}.dump"
 CONTAINER_NAME="postgres"
 CONTAINER_TMP_RESTORE_DIR="/tmp/"
 
-# Load environment variables from .env file
-set -o allexport # Automatically exports all variables defined after this line into the environment.
-source .env # Loads the variables from the .env file.
-set +o allexport # Disables automatic export of variables.
-
-# Check input parameter
+# Check path to backup parameter
 if [ -z "$1" ]; then
   echo "❌ Error: You must provide the path to the backup directory."
   echo "Usage: $0 /path/to/backup"
   exit 1
 fi
 
+# Check path to backup parameter
+if [ -z "$2" ]; then
+  echo "❌ Error: You must provide the database username."
+  echo "Usage: $0 username"
+  exit 1
+fi
+
 BACKUP_DIR="$1"
+POSTGRES_USER="$2"
 
 # Create folder if does not exists yet
 mkdir -p "$BACKUP_DIR"
@@ -28,10 +31,16 @@ mkdir -p "$BACKUP_DIR"
 docker exec -t "$CONTAINER_NAME" pg_dump -Fc -v -f "$CONTAINER_TMP_RESTORE_DIR$FILENAME" -U "$POSTGRES_USER" "$DB_NAME"
 
 # Download backup generated
-docker cp "$CONTAINER_NAME:$CONTAINER_TMP_RESTORE_DIR$FILENAME" "$BACKUP_DIR"
+docker cp "$CONTAINER_NAME:$CONTAINER_TMP_RESTORE_DIR$FILENAME" backups
 
-# (Optional) Remove backups older than 90 days
-find "$BACKUP_DIR" -type f -name "*.dump" -mtime +90 -exec rm {} \;
+# Move the generated dump to the destination backup directory
+mv backups/"$FILENAME" "$BACKUP_DIR"
+
+# Compress the generated dump
+gzip "$BACKUP_DIR/$FILENAME"
+
+# Remove the temp files
+docker exec "$CONTAINER_NAME" rm -rf "$CONTAINER_TMP_RESTORE_DIR$FILENAME"
 
 # Display a success message
 echo "Backup done: $BACKUP_DIR/$FILENAME"
